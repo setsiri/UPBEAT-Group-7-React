04f69c0721cea4a898972ab49cbf9c75a60899dc
setsiri matewin <setsiri_matewin@cmu.ac.th>
2023-03-12 05:10:57 +0700

    margin update

--- nextjs-react-web/pages/Part2Gameplay/UpdateConstructionPlan.js
+++ nextjs-react-web/pages/Part2Gameplay/UpdateConstructionPlan.js
@@ -5,12 +5,12 @@ import Head from 'next/head'
 
 function UpdateConstructionPlan() {
   return (
-    <div class="d-grid gap-2 col-1 mx-5">
+    <div class="d-grid gap-2 col-1 mx-5 ">
       <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
       </Head>
-      <h2>UpdateConstructionPlan</h2>
+      <h2 class="my-2">UpdateConstructionPlan</h2>
         
         <h5>contructionplan editor</h5>
         <div><textarea
