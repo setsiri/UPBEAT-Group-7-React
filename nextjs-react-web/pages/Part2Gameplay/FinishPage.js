bb111a2bd8ef048c2d0e7c516d18a5935541fdc8
setsiri matewin <setsiri_matewin@cmu.ac.th>
2023-03-12 05:07:09 +0700

    js

--- nextjs-react-web/pages/Part2Gameplay/FinishPage.js
+++ nextjs-react-web/pages/Part2Gameplay/FinishPage.js
@@ -5,15 +5,19 @@ import Head from 'next/head'
 
 function FinishPage() {
   return (
-    <div>
+    <div class="d-grid gap-2 col-1 mx-5 my-2">
       
 <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-      </Head><h1>FinishPage</h1>
-        <li> 
-        <Link href="/">go to homepage</Link>
-        </li>
+      </Head><h2>FinishPage</h2>
+ <p>the winner is player 1 !!</p>
+
+      <div >
+      <button class="btn btn-primary my-3"><Link href="/">go to homepage</Link></button>
+      
+</div>
+        
     </div>
   )
 }
