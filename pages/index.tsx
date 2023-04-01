2c68a6b52e439e04a99ab50c0743dfb65a8a5e12
setsiri <76035222+setsiri@users.noreply.github.com>
2023-04-01 23:56:08 +0700

    push homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,26 +40,26 @@ export default function Home() {
   width: "100vw",
   height: "100vh"
 }}><Image src={bg} alt={""}  />
-</div>
+</div> 1
 */}
 
           <div className="text-center mx-auto ">
             <Image src={mypic2} alt={""} />
           </div>
 
-        <div className="text-center my-4 text-black ">
-          <h3 color="dark">Homepage</h3>
+          <div className="text-center my-4 text-black ">
+            <h3 color="dark">Homepage</h3>
+          </div>
+          <div className="d-grid gap-2 col-1 mx-auto ">
+            <Link href="/HowtoPlay">
+              <button className="btn btn-primary">how to play</button>
+            </Link>
+            <Link href="/game_setup/SetConfigurationPlan">
+              <button className="btn btn-primary">start game</button>
+            </Link>
+          </div>
         </div>
-        <div className="d-grid gap-2 col-1 mx-auto ">
-          <Link href="/HowtoPlay"><button className="btn btn-primary">
-            how to play
-          </button></Link>
-          <Link href="/game_setup/SetConfigurationPlan"><button className="btn btn-primary">
-            start game
-          </button></Link>
-        </div>
-      </div>
-    </motion.div></AnimatePresence>
-   
+      </motion.div>
+    </AnimatePresence>
   );
 }
